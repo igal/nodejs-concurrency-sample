c45130dd6a0fee04faeef0d5cd0eea16ec727623
Igal Koshevoy <igal@pragmaticraft.com>
2011-05-05 17:08:31 -0700

    Add examples using the 'seq' library and much documentation.

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var util = require('util');
 var express = require('express')
 var app = module.exports = express.createServer();
 var sys = require('sys');
-// TODO var Worker = require('webworker').Worker;
-// var Task = require('parallel').Task;
+var Seq = require('seq');
 
 // Configuration
 app.configure(function(){
@@ -25,17 +24,38 @@ app.configure(function(){
 });
 
 // Database
-var MySQLPool = require("mysql-pool").MySQLPool;
+var MySQLPool = require('mysql-pool').MySQLPool;
 var db = new MySQLPool({});
 db.properties.user = DATABASE_USER;
 db.properties.password = DATABASE_PASSWORD;
 db.connect(DATABASE_POOL_SIZE);
 
+// Return length of array. Returns number of keys in an associative array or the number of elements in a regular array.
+function array_length(array) {
+  var count = 0;
+  for (item in array) {
+    count++;
+  }
+  return count;
+}
+
+// Return an array of database names.
+function rows_to_names(rows) {
+  var names = [];
+  for (key in rows) {
+    names.push(rows[key]['Database']);
+  }
+  return names;
+}
+
+// Display homepage
 app.get('/', function(req, res){
   res.render('index', {title: 'Hello!'});
 });
 
-app.get('/sequential', function(req, res){
+// Demonstrate sequential calls using nested callbacks. One query is run, which
+// runs the next, which runs the next. The final query renders the page.
+app.get('/sequential-callbacks', function(req, res){
   var number_of_calls = 2;
   var sleep_time = 0.2;
   var expected_time = number_of_calls * sleep_time;
@@ -45,25 +65,74 @@ app.get('/sequential', function(req, res){
     db.query('select sleep('+sleep_time+')', function(err2, rows2, fields2) {
       db.query('show databases', function(err3, rows3, fields3) {
         res.render('page', {
-          title: 'Sequential calls',
+          title: 'Sequential calls using callbacks',
           elapsed_time: (new Date() - t)/1000,
           expected_time: expected_time,
           number_of_calls: number_of_calls,
-          rows: rows3
+          names: rows_to_names(rows3)
         });
       });
     });
   });
 });
 
-app.get('/parallel', function(req, res){
+// Demonstrates sequential calls using the Seq library. It manages execution of
+// functions, running each one and passing along the results, and the last one
+// renders the page. This seems much clearer than using nested callbacks.
+app.get('/sequential-with-seq', function(req, res){
+  var number_of_calls = 2;
+  var sleep_time = 0.2;
+  var expected_time = number_of_calls * sleep_time;
+  var t = new Date();
+
+  Seq()
+    .seq(function(){
+      console.log('sequential-with-seq: starting sleeper #1');
+      db.query('select sleep('+sleep_time+')', this);
+    })
+    .seq(function(rows, fields){
+      console.log('sequential-with-seq: finished sleeper #1, got '+rows.length+' rows');
+      console.log('sequential-with-seq: starting sleeper #2');
+      db.query('select sleep('+sleep_time+')', this);
+    })
+    .seq(function(rows, fields){
+      console.log('sequential-with-seq: finished sleeper #2, got '+rows.length+' rows');
+      console.log('sequential-with-seq: starting name query');
+      db.query('show databases', this);
+    })
+    .seq(function(rows, fields){
+      console.log('sequential-with-seq: finished name query, got '+rows.length+' rows');
+      res.render('page', {
+        title: 'Sequential calls using Seq library',
+        elapsed_time: (new Date() - t)/1000,
+        expected_time: expected_time,
+        number_of_calls: number_of_calls,
+        names: rows_to_names(rows)
+      });
+    })
+  ;
+});
+
+// This action demonstrates parallel execution using callbacks. The code is
+// unusual because it starts a bunch of asynchronous queries that run in
+// parallel, and each of these is responsible for uploading its results to a
+// collector. When the collector has all the results, it renders the page.
+app.get('/parallel-collector', function(req, res){
   var number_of_calls = DATABASE_POOL_SIZE-1;
   var sleep_time = 0.2;
   var expected_time = sleep_time;
   var t = new Date();
 
-  var results = {sleepers: {}, rows: null};
+  // Results of queries as populated by the `collector`.
+  var results = {
+    sleepers: {},
+    rows: null
+  };
+
+  // Populates the `results` data structure. This function is called by the
+  // queries when they're done.
   var collector = function(name, payload) {
+    // Store results
     if (name == 'sleep') {
       console.log('parallel: Sleeper #'+payload+' finished');
       results.sleepers[payload] = true;
@@ -72,35 +141,73 @@ app.get('/parallel', function(req, res){
       results.rows = payload;
     }
 
-    var sleepers_finished = 0;
-    for (key in results.sleepers) { sleepers_finished++; }
-    if (results.rows && sleepers_finished >= number_of_calls) {
+    // Render page when done
+    if (results.rows && array_length(results.sleepers) >= number_of_calls) {
       res.render('page', {
-        title: 'Parallel calls',
+        title: 'Parallel calls using callback collector',
         elapsed_time: (new Date() - t)/1000,
         expected_time: expected_time,
         number_of_calls: number_of_calls,
-        rows: results.rows
+        names: rows_to_names(results.rows)
       });
     }
   }
 
+  // Asynchronous query to get names, will hand off results to the `collector` when done.
   var get_names = function() {
     db.query('show databases', function(err, rows, fields) {
       collector('names', rows);
     });
   }
 
+  // Asynchronous query to sleep for a while, will contact the `collector` when done.
   var get_sleep = function(identifier) {
     db.query('select sleep('+sleep_time+')', function(err, rows, fields) {
       collector('sleep', identifier);
     });
   }
 
+  // Start the queries. The `collector` will capture their results and render
+  // the page when they finish.
   for (var i = 0; i<number_of_calls; i++) {
     get_sleep(i);
   }
   get_names();
+});
+
+// This action demonstrates parallel queries using the Seq library. This is
+// significantly clearer than using callbacks and a collector.
+app.get('/parallel-with-seq', function(req, res){
+  var number_of_calls = DATABASE_POOL_SIZE-1;
+  var sleep_time = 0.2;
+  var expected_time = sleep_time;
+  var t = new Date();
+
+  var chain = Seq();
+  for (var i = 0; i < number_of_calls; i++) {
+    var identity = 'sleeper#'+i;
+    chain.par(identity, function() {
+      console.log('parallel-with-seq: starting sleeper');
+      db.query('select sleep('+sleep_time+')', this);
+    });
+  }
+  chain.par('names', function(){
+    // Note the use of 'names' in the line above, this captures the query's
+    // results into a named entry in the stack, which can be accessed later as
+    // `this.vars['names']`.
+    console.log('parallel-with-seq: starting name query');
+    db.query('show databases', this);
+  });
+  chain.seq(function(rows, fields){
+    console.log('parallel-with-seq: found '+array_length(this.vars)+' finished processes');
+    res.render('page', {
+      title: 'Parallel calls using Seq library',
+      elapsed_time: (new Date() - t)/1000,
+      expected_time: expected_time,
+      number_of_calls: number_of_calls,
+      names: rows_to_names(this.vars['names'])
+    });
+  })
 });
 
 var PORT = 3000;
